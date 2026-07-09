909a6387cd553d44d53b8f78eaba184c00f5a226
Kapil Gaire <eriaglipak312@gmail.com>
2026-07-09 11:52:23 +0545

    added test slash commands

---
+++ main.py
@@ -0,0 +1,80 @@
+import json
+import os
+from dis import disco
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+
+client = discord.Client(intents=intents)
+
+# initialize tree to use slash commands
+tree = discord.app_commands.CommandTree(client=client)
+
+
+# targeting a specific server is faster when developing.
+GUILD = discord.Object(id=1413015897039306786)
+
+
+@tree.command()
+async def test(interaction):
+    await interaction.response.send_message("sup")
+
+
+@client.event
+async def on_ready():
+    try:
+        await tree.sync(
+            guild=GUILD
+        )  # register the slash command  #initializing the slash command in specific sever only
+
+    except Exception as e:
+        print(e)
+    print(f"ready: {client.user}")
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    print(message.content)
+    first_word = message.content.split(maxsplit=1)[0]
+    application_id = "<@" + str(client.application_id) + ">"
+
+    try:
+        if first_word == application_id:
+            print("true")
+            split_words = message.content.split(maxsplit=2)
+            print(split_words)
+
+            match split_words[1]:
+                case "--help":
+                    await message.channel.send("So you need help, huh?")
+
+                case "--format":
+                    await message.channel.send("format of what, huh?")
+
+                case _:
+                    raise ValueError("Choose a valid option")
+
+        else:
+            raise ValueError("Choose a valid option")
+    except ValueError:
+        await message.channel.send(
+            "Enter a valid command. Use '@HisabBot --help' for help."
+        )
+
+    except Exception as e:
+        print(e)
+        await message.channel.send("Failed to make changes, erorr occures.")
+
+
+token = os.getenv("BOT_TOKEN")
+client.run(token)

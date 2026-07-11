e4a02b8f6e87c1119305bd094261fe9dd485b229
Kapil Gaire <eriaglipak312@gmail.com>
2026-07-11 15:21:34 +0545

    Added parser to get particiant id and share amount

--- utils/custom_errors.py
+++ utils/custom_errors.py
@@ -1,7 +1,22 @@
 class DatabaseCreationFailedError(Exception):
     """Error raised when it fails to create databse and tables."""
+
     pass
+
 
 class UserTableInitializeError(Exception):
     """Error raised when it fails to add users to the users table."""
+
+    pass
+
+
+class UserIdsFetchError(Exception):
+    """Error raised when it fails to fetch the users ids."""
+
+    pass
+
+
+class ExpenseSaveError(Exception):
+    """Errror raised when it fails to save the expense or participants"""
+
     pass
--- utils/utils.py
+++ utils/utils.py
@@ -17,4 +17,29 @@ def get_member_id_from_string(members_str: str):
         except ValueError:
             pass
 
-    return members
+    # remove duplicates.
+    unique_members = list(set(members))
+
+    return unique_members
+
+
+def get_member_id_and_share(values_string: str):
+    matches = re.findall(r"<@(\d+)>\s*:\s*(\d+(?:\.\d+)?)", values_string)
+    id_share_pair = []
+    participant_ids = []
+
+    for member_id, share in matches:
+        try:
+            int_id = int(member_id)
+            float_share = float(share)
+
+            if member_id in participant_ids:  # prevent duplicte value of same user
+                continue
+
+            id_share_pair.append({"id": int_id, "share": float_share})
+            participant_ids.append(int_id)
+
+        except ValueError:
+            pass
+    print(id_share_pair)
+    return (id_share_pair, participant_ids)
